1cea36b8516bb6aad4b3f04d69ba2dbe5c765443
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2020-11-19 08:08:40 -0700

    fix rubocop errors

--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -1,12 +1,12 @@
 require_relative '../lib/messages'
 
 describe Messages do
-  let (:message) { Messages.new("Mock") }
-  describe "messages object" do
-    it "has greeting instance variable" do
-      expect(message.greeting).to be_a(String) 
+  let(:message) { Messages.new('Mock') }
+  describe 'messages object' do
+    it 'has greeting instance variable' do
+      expect(message.greeting).to be_a(String)
     end
-    it "has invalid_command instance variable" do
+    it 'has invalid_command instance variable' do
       expect(message.invalid_command).to be_a(String)
     end
   end
--- spec/quote_spec.rb
+++ spec/quote_spec.rb
@@ -1,10 +1,10 @@
 require_relative '../lib/quote'
 
 describe Quote do
-  let (:quote) { Quote.new }
-  describe "quote object" do
-    it "returns an array" do
-      expect(quote.return_quote).to be_a(Array) 
+  let(:quote) { Quote.new }
+  describe 'quote object' do
+    it 'returns an array' do
+      expect(quote.return_quote).to be_a(Array)
     end
   end
 end
